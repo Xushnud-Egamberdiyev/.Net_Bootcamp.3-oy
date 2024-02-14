6bece6cd52efd115a9021d846d9a0e38694527a6
egamberdiyevxushnud <xushnudegamberdiyev0@gmail.com>
2024-02-14 22:31:18 +0500

    Ado net Get Create

---
+++ 45-dars_WepApi/Controllers/ExperimentsController.cs
@@ -0,0 +1,70 @@
+﻿using _45_dars_WepApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace _45_dars_WepApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ExperimentsController : ControllerBase
+    {
+        const string connectionstring = "Server=127.0.0.1;Port=5432;Database=MongoDb;username=postgres;Password=xushnud;";
+
+        [HttpGet]
+        public List<Experement> Get()
+        {
+            using(NpgsqlConnection  connection = new NpgsqlConnection(connectionstring))
+            {
+                string query = $"Select * from darslar;";
+                connection.Open();
+
+                NpgsqlCommand command =new NpgsqlCommand(query,connection);
+
+                var x = command.ExecuteReader();
+
+                List<Experement> list = new List<Experement>();
+
+                while(x.Read())
+                {
+                    list.Add(new Experement()
+                    {
+                        Id = (int)x[0],
+                        mavzu = (string)x[1],
+                        student_count = (int)x[2]
+                    });
+
+                }
+
+                return list;
+            }
+        }
+
+
+
+        [HttpPost]
+        public string Create(string mavzu, int student_count)
+        {
+            using(NpgsqlConnection connection = new NpgsqlConnection(connectionstring))
+            {
+                string query = $"insert into darslar(mavzu, student_count) values(@mavzu, @student_count)";
+                connection.Open();
+
+                NpgsqlCommand command =new NpgsqlCommand(query,connection);
+
+                command.Parameters.AddWithValue("mavzu", mavzu);
+                command.Parameters.AddWithValue("student_count", student_count);
+
+                int x = command.ExecuteNonQuery();
+
+                if(x != 0)
+                {
+                    return "Malumot yaratildi";
+                }
+                return "Hech narsa qo'shilmadi";
+
+            }
+        }
+
+    }
+}
---
+++ 45-dars_WepApi/Models/Experement.cs
@@ -0,0 +1,9 @@
+﻿namespace _45_dars_WepApi.Models
+{
+    public class Experement
+    {
+        public int Id { get; set; }
+        public string mavzu { get; set; }
+        public int student_count { get; set; }
+    }
+}
