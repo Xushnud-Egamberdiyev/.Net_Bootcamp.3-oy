c0b758bcc210850460e7c63cb053fa3c85b09112
egamberdiyevxushnud <xushnudegamberdiyev0@gmail.com>
2024-02-21 15:25:54 +0500

    update entity

---
+++ 51-Entity/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using _51_Entity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _51_Entity.Infrastructure
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<car> Cars { get; set; }
+    }
+}
---
+++ 51-Entity/Sercise/ICarSercise.cs
@@ -0,0 +1,9 @@
+﻿using _51_Entity.Models;
+
+namespace _51_Entity.Sercise
+{
+    public interface IcarSercise
+    {
+        public Task<string> CreateCarAsync(car model);
+    }
+}
