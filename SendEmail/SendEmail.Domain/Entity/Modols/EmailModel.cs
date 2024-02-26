904d45662691a08b7e1fda6adb5009c833ab821f
egamberdiyevxushnud <xushnudegamberdiyev0@gmail.com>
2024-02-26 15:26:34 +0500

    update

---
+++ SendEmail/SendEmail.Domain/Entity/Modols/EmailModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendEmail.Domain.Entity.Modols
+{
+    public class EmailModel
+    {
+    }
+}
